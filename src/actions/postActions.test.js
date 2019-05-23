647e7d72f9003bf0691177b98d6809a248abdcde
Mallarie <mallariemilano@gmail.com>
2019-05-23 10:08:44 -0700

    created deletePost action and passed the test

--- src/actions/postActions.test.js
+++ src/actions/postActions.test.js
@@ -1,10 +1,16 @@
-import { CREATE_POST, createPost } from './postActions';
+import { CREATE_POST, createPost, DELETE_POST, deletePost } from './postActions';
 
 describe('post actions', () => {
   it('creates a create post action', () => {
     expect(createPost('My Post', 'Sup?')).toEqual({
       type: CREATE_POST,
       payload: { title: 'My Post', body: 'Sup?' }
+    });
+  });
+  it('creates a delete post action', () => {
+    expect(deletePost(0)).toEqual({
+      type: DELETE_POST,
+      payload: 0
     });
   });
 });
