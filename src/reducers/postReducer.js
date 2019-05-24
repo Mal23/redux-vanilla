d3a3ce044f60b8b465cf4e52097049d7f827f4e2
Mallarie <mallariemilano@gmail.com>
2019-05-23 17:14:31 -0700

    handles the delete post action

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { CREATE_POST } from '../actions/postActions';
+import { CREATE_POST, DELETE_POST } from '../actions/postActions';
 
 const initialState = [];
 
@@ -6,7 +6,11 @@ export default function reducer(state = initialState, action) {
   switch(action.type) {
     case CREATE_POST:
       return [...state, action.payload];
-
+    case DELETE_POST:
+      return [
+        ...state.slice(0, action.payload),
+        ...state.slice(action.payload + 1)
+      ];
     default:
       return state;
   }
--- src/reducers/postReducer.test.js
+++ src/reducers/postReducer.test.js
@@ -1,9 +1,21 @@
 import reducer from './postReducer';
-import { createPost } from '../actions/postActions';
+import { createPost, deletePost } from '../actions/postActions';
 
 describe('post reducer', () => {
   it('handles the CREATE_POST action', () => {
     const newState = reducer([], createPost('My post', 'So entertaining!'));
+
+    expect(newState).toEqual([
+      { title: 'My post', body: 'So entertaining!' }
+    ]);
+  });
+
+  it('handles the DELETE_POST action', () => {
+    const initialState = [
+      { title: 'My post', body: 'So entertaining!' },
+      { title: 'Another post', body: 'Informative' }
+    ];
+    const newState = reducer(initialState, deletePost(1));
 
     expect(newState).toEqual([
       { title: 'My post', body: 'So entertaining!' }
