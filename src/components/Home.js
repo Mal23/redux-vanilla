3340c2a68175515d91d1c1addb4b498439975832
Mallarie <mallariemilano@gmail.com>
2019-05-28 09:37:07 -0700

    added styling to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import CreatePost from '../containers/posts/CreatePost';
 import AllPosts from '../containers/posts/AllPosts';
+import styles from './Home.css';
 
 export default function Home() {
   return (
     <>
       <h1>Posts</h1>
-      <h2>Create Post</h2>
-      <CreatePost />
+      <section className={styles.CreatePost}>
+        <h2>Create Post</h2>
+        <CreatePost />
+      </section>
       <section>
         <AllPosts />
       </section>
