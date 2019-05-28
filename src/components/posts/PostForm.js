2c8dfee9e6601aabf3227de65e7425be78aef28d
Mallarie <mallariemilano@gmail.com>
2019-05-28 09:41:24 -0700

    added styling to post form

--- src/components/posts/PostForm.js
+++ src/components/posts/PostForm.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { FaCheck } from 'react-icons/fa';
+import styles from './PostForm.css';
 
 export default class PostForm extends PureComponent {
   static propTypes = {
@@ -27,7 +28,7 @@ export default class PostForm extends PureComponent {
   render() {
     const { title, body } = this. state;
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form className={styles.PostForm} onSubmit={this.handleSubmit}>
         <input name="title" value={title} onChange={this.handleChange} />
         <textarea name="body" value={body} onChange={this.handleChange}></textarea>
         <button>Submit <FaCheck /></button>
