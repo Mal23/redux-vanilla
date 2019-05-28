2258ed96f651f0b51e45b53714d49fc3a18de028
Mallarie <mallariemilano@gmail.com>
2019-05-27 21:05:38 -0700

    added postById

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import PostById from '../containers/posts/PostById';
 import Home from './Home';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
     <Router>
       <Link to="/">Home</Link>
       <Switch>
+        <Route path="/posts/:id" component={PostById} />
         <Route path="/" component={Home} />
       </Switch>
     </Router>
