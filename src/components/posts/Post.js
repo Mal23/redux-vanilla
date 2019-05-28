89b5b735edc452c7c722c65c8981257057ceaf2f
Mallarie <mallariemilano@gmail.com>
2019-05-27 21:21:07 -0700

    deletePost

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { FaTrashAlt } from 'react-icons/fa';
 
-function Post({ post }) {
+function Post({ post, deletePost }) {
   return (
     <>
       <Link to={`/posts/${post.id}`}>
         {post.title}
       </Link>
+      <FaTrashAlt onClick={deletePost.bind(null, post.id)} />
     </>
   );
 }
 
 Post.propTypes = {
+  deletePost: PropTypes.func.isRequired,
   post: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired
